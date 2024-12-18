4352a845bdfb0a5d371c13504c0bf553a402e0f7
Ckott <diegoangeles130703@gmail.com>
2024-12-17 22:35:52 -0500

    base proyect

---
+++ src/components/card-products.jsx
@@ -0,0 +1,21 @@
+import "./card-products.css"
+
+export const CardProducts = ({title, price, image, category, description}) => {
+    return (
+        <div className="card-product" aria-label="card-products">
+            <div className="card-image-container">
+                <img className="card-product-image" src={image} alt={title}/>
+            </div>
+            <article className="card-container-article">
+                <div className="card-article">
+                    <h2 className="card-product-title">{title}</h2>
+                    <span className="card-product-badged">{category}</span>
+                </div>
+                <div>
+                    <p>s/ {price}</p>
+                </div>
+                <p className="card-product-discription">{description}</p>
+            </article>
+        </div>
+    )
+}
